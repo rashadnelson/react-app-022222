920603193b37b3eda57a7013c954fa987fef1420
Rashad Nelson <rashadnelson@gmail.com>
2022-02-23 08:02:33 -0500

    First commit - 02/23/22

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react' // Importing React from React
+import ReactDom from 'react-dom';  // Importing ReactDom, as it contains the render() function
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Stateless functional component
+// Always returns JSX.  Include ()
+// Capitalize the name of the function making it a React component
+// Close every element with a />
+// Can only return one complete element - place everything inside a <div>
+// Must use camelCase for HTML attributes
+// className instead of class
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting() {
+
+  // The JSX being returned
+  return (
+    <section className="first-component">
+      <h4>This is Rashad and this is my
+        React component</h4>;
+      <ul>
+        <li>
+          <a href="#">Hello People</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+/*
+// Another way to do it, but not ideal.  
+// Passes three things.
+// 1. The HTML element
+// 2. The props object (to be reviewed)
+// 3. The content inside the HTML element
+const Greeting = () => {
+  return React.createElement("h1", {}, "This is my second React component")
+}
+*/
+
+// The "root" element in public > index.html is now tied to the "Greeting" component. 
+// <What we're going to render/>, Where we're going to render>
+ReactDom.render(<Greeting />, document.getElementById('root'));
+
